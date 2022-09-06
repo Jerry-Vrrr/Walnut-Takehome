3a7c277c76e866a0c4ac69737983aa271dfb44c0
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-09-06 15:55:26 -0500

    Generate boxes and hover effect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import BoxContainer from './Components/BoxContainer';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+const [numBoxes, setNumBoxes] = useState(25)
+
   return (
     <div className="App">
-
+      <BoxContainer numBoxes={numBoxes}/>
       
     </div>
   );
--- src/Components/BoxContainer.js
+++ src/Components/BoxContainer.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import Box from './Box'
+import './BoxContainer.css'
 
-export default function BoxContainer() {
+export default function BoxContainer( {numBoxes}) {
+
+    const boxes = []
+    for (let i=0; i < numBoxes; i++) {
+        boxes.push(`${i+1}`)
+    }
+    const generate = boxes.map(box => {
+       return  (
+           <Box id={box} key={box}/>
+       )
+    })
+
   return (
-    <div>BoxContainer</div>
+    <div className='container'>
+        {generate}
+        
+    </div>
   )
 }
